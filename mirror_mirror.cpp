06da08489b36e4b353bcc59e41bf7e116ce0c167
Vaishnavi <vaishnavipatil0248@gmail.com>
2023-04-05 01:00:43 +0530

    62

---
+++ mirror_mirror.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    string s;
+    cin>>s;
+    string s1=s;
+    int l,r;
+    cin>>l>>r;
+    int index=r-1;
+    for(int i=l-1;i<r;i++)
+    {
+        s1[index]=90-s[i]+65;
+        index--;
+    }
+    cout<<s1;
+    return 0;
+}
